acead70ba3a16c7bc3cbfce793af379576b7654f
Alex <romanov123@yandex.ru>
2023-03-31 22:38:45 +0300

    j

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число N:");
+int N = int.Parse (Console.ReadLine());
+
+if ((N/10000) == N%10 && ((N/1000)%10 == ((N%100)/10)))
+{Console.Write ($" Число N является палиндромом ");}
+else
+{Console.Write ($" Число N не является палиндромом ");}
